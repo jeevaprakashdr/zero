43835f5a9dec9139fcb3813155156091e332badf
jeeva <jeevaprakashdr@gmail.com>
2026-08-20 14:31:46 +0200

    implement iter and iter_mut for RingBuffer

--- src/ring_buffer/mod.rs
+++ src/ring_buffer/mod.rs
@@ -1,4 +1,4 @@
-use core::{marker::PhantomData, mem::MaybeUninit, slice};
+use core::{marker::PhantomData, mem::MaybeUninit};
 
 pub struct RingBuffer<T, const N: usize> {
     _marker: PhantomData<T>,
@@ -64,6 +64,84 @@ impl<T, const N: usize> RingBuffer<T, N> {
         }
     }
 
+    fn iter(&self) -> Iter<'_, T, N> {
+        Iter {
+            rb: self,
+            index: 0,
+            len: self.len(),
+        }
+    }
+
+    fn iter_mut(&mut self) -> IterMut<'_, T, N> {
+        let len = self.len();
+        IterMut {
+            rb: self,
+            index: 0,
+            len,
+        }
+    }
+}
+
+impl<'a, T, const N: usize> IntoIterator for &'a RingBuffer<T, N> {
+    type Item = &'a T;
+
+    type IntoIter = Iter<'a, T, N>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T, const N: usize> IntoIterator for &'a mut RingBuffer<T, N> {
+    type Item = &'a mut T;
+
+    type IntoIter = IterMut<'a, T, N>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+pub struct Iter<'a, T, const N: usize> {
+    rb: &'a RingBuffer<T, N>,
+    index: usize,
+    len: usize,
+}
+
+pub struct IterMut<'a, T, const N: usize> {
+    rb: &'a mut RingBuffer<T, N>,
+    index: usize,
+    len: usize,
+}
+
+impl<'a, T, const N: usize> Iterator for Iter<'a, T, N> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.len {
+            let collection_start_ptr: *const T = self.rb.data.as_ptr().cast::<T>();
+            let item_ptr = unsafe { collection_start_ptr.add(self.rb.head + self.index) };
+            self.index = (self.index + 1) % N;
+            Some(unsafe { &*item_ptr })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T, const N: usize> Iterator for IterMut<'a, T, N> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.len {
+            let collection_start_ptr: *mut T = self.rb.data.as_mut_ptr().cast::<T>();
+            let item_ptr = unsafe { collection_start_ptr.add(self.rb.head + self.index) };
+            self.index = (self.index + 1) % N;
+            Some(unsafe { &mut *item_ptr })
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -152,4 +230,48 @@ mod tests {
         assert_eq!(rb.len(), 0);
     }
 
+    #[test]
+    fn iter() {
+        let mut rb: RingBuffer<i8, 4> = RingBuffer::new();
+        let _ = rb.enqueue(1);
+        let _ = rb.enqueue(2);
+        let _ = rb.enqueue(3);
+
+        let mut items = rb.iter();
+
+        assert_eq!(items.next(), Some(&1));
+        assert_eq!(items.next(), Some(&2));
+        assert_eq!(items.next(), Some(&3));
+        assert_eq!(items.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut rb: RingBuffer<i8, 4> = RingBuffer::new();
+        let _ = rb.enqueue(1);
+        let _ = rb.enqueue(2);
+        let _ = rb.enqueue(3);
+
+        let mut items = rb.iter_mut();
+
+        assert_eq!(items.next(), Some(&mut 1));
+        assert_eq!(items.next(), Some(&mut 2));
+        assert_eq!(items.next(), Some(&mut 3));
+        assert_eq!(items.next(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut rb: RingBuffer<i8, 4> = RingBuffer::new();
+        let _ = rb.enqueue(1);
+        let _ = rb.enqueue(2);
+        let _ = rb.enqueue(3);
+
+        let mut items = rb.into_iter();
+
+        assert_eq!(items.next(), Some(&1));
+        assert_eq!(items.next(), Some(&2));
+        assert_eq!(items.next(), Some(&3));
+        assert_eq!(items.next(), None);
+    }
 }

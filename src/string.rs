15666396a546ac43393356ed4455c31fd9ec553a
jeeva <jeevaprakashdr@gmail.com>
2026-08-21 23:01:17 +0200

    implemented fn for  push and from_utf8

--- src/string.rs
+++ src/string.rs
@@ -1,4 +1,4 @@
-use core::slice;
+use core::{slice, str::Utf8Error};
 
 use crate::Vec;
 
@@ -31,6 +31,29 @@ impl<const N: usize> String<N> {
         new
     }
 
+    fn from_utf8(v: Vec<u8, N>) -> Result<String<N>, Utf8Error> {
+        {
+            let ptr = v.data.as_ptr() as *const u8;
+            let slice = unsafe { slice::from_raw_parts(ptr, v.len) };
+            str::from_utf8(slice)?;
+        }
+
+        Ok(String { vec: v })
+    }
+
+    fn from_utf8_unchecked(v: Vec<u8, N>) -> String<N> {
+        String { vec: v }
+    }
+    
+    fn push(&mut self, c: char) -> Result<(), Error> {
+        if self.vec.len + 1 <= self.vec.capacity() {
+            let _ = self.vec.push(c as u8);
+            Ok(())
+        } else {
+            Err(Error::Full)
+        }
+    }
+
     pub fn push_str(&mut self, s: &str) -> Result<(), Error> {
         let bytes = s.bytes();
         if self.vec.len + bytes.len() <= self.vec.capacity() {
@@ -58,12 +81,13 @@ impl<const N: usize> String<N> {
         let slice = unsafe { slice::from_raw_parts_mut(ptr, self.vec.len) };
         unsafe { core::str::from_utf8_unchecked_mut(slice) }
     }
+
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::string::String;
+    use crate::{Vec, string::String};
 
     #[test]
     fn new() {
@@ -79,6 +103,36 @@ mod tests {
 
         assert!(!s.is_empty());
         assert_eq!(s.len(), 3);
+    }
+
+    #[test]
+    fn from_utf8() {
+        let mut v: Vec<u8, 3> = Vec::new();
+        let _ = v.push('a' as u8);
+        let _ = v.push('b' as u8);
+
+        let s = String::from_utf8(v).unwrap();
+        assert_eq!(s.as_str(), "ab");
+    }
+
+    #[test]
+    fn from_utf8_unchecked() {
+        let mut v: Vec<u8, 3> = Vec::new();
+        let _ = v.push(0);
+
+        let v = String::from_utf8_unchecked(v);
+
+        assert!(!v.is_empty());
+        assert_eq!(v.len(), 1);
+    }
+
+    #[test]
+    fn push() {
+        let mut s: String<4> = String::from("abc");
+
+        assert!(s.push('1').is_ok());
+        assert_eq!(s.as_str(), "abc1");
+        assert_eq!(s.len(), 4);
     }
 
     #[test]
